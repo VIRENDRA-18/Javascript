3584e562d62ee85fc9a7133e17d1d874db1043f2
VIRENDRA-18 <virusingh0125@gmail.com>
2025-12-02 23:26:36 +0530

    Add string methods examples and update script reference in HTML

---
+++ app_module_3.js
@@ -0,0 +1,27 @@
+// string Methods in JavaScript
+
+//Trim Method - It removes whitespace from both sides of a string
+
+let msg = "    Hello  Virendra!           ";
+console.log(msg.trim());
+
+// Example of trim method
+
+let password = prompt("set your password");
+let NewPassword = password.trim();
+console.log(NewPassword);
+
+//toUpperCase() Method - It converts string to uppercase letters
+//toLowerCase() Method - It converts string to lowercase letters
+
+let Name = "Virendra Singh";
+console.log(Name);
+console.log(Name.toUpperCase());
+console.log(Name.toLowerCase());
+
+//indexOf() Method - It returns the index of the first occurrence of a specified text in a string
+let fruit = "Apple Banana Mango Orange Apple";
+let index = fruit.indexOf("Mango");
+let index1 = fruit.indexOf("na");
+console.log( index);
+console.log( index1);
