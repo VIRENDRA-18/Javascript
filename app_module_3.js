0b60d627f410abdd43414e101ebdb61bd796a208
VIRENDRA-18 <virusingh0125@gmail.com>
2025-12-03 00:46:38 +0530

    string method replace and repeat, and more practice question

--- app_module_3.js
+++ app_module_3.js
@@ -7,9 +7,9 @@ console.log(msg.trim());
 
 // Example of trim method
 
-let password = prompt("set your password");
-let NewPassword = password.trim();
-console.log(NewPassword);
+// let password = prompt("set your password");
+// let NewPassword = password.trim();
+// console.log(NewPassword);
 
 //toUpperCase() Method - It converts string to uppercase letters
 //toLowerCase() Method - It converts string to lowercase letters
@@ -37,3 +37,36 @@ let city = "     New Delhi     ";
 //Method chainning
 let NewCity = city.trim().toUpperCase();
 console.log("After method chainning :",NewCity);
+
+//slice() Method - It extracts a section of a string and returns it as a new string
+
+let country = "United States of America";
+let NewCountry = country.slice(0,6); // extracts "United"s
+let NewCountry1 = country.slice(-7); // extracts "America"
+console.log(NewCountry1);
+console.log(NewCountry);
+
+
+//replace and repeat Methods in JavaScript
+
+let Msg = "Hii Viru, How are you ?";
+console.log(Msg);
+console.log(Msg.replace("Viru","KumKum")); 
+console.log(Msg.repeat(3));
+
+let Website = "New-Look-Hub";
+console.log(Website.repeat(2));
+
+//Practice Question
+
+//question 1;
+
+let msg1 = "help!";
+console.log(msg1.trim().toUpperCase());
+
+//question 2;
+
+let name = "ApnaCollege";
+console.log(name.slice(4,9));
+console.log(name.indexOf("na"));
+console.log(name.replace("Apna","Our"));
