2eec820929f9b112e5937a706ad2eda86a2c9f8d
VIRENDRA-18 <virusingh0125@gmail.com>
2025-12-03 11:11:03 +0530

    Update index.html to reference appArray_module_3.js and remove old script

---
+++ appArray_module_3.js
@@ -0,0 +1,9 @@
+let students = ["Virendra", "KumKum", "Anjali", "Ravi", "Amit"];
+console.log(students);
+ 
+
+// array are mutable
+let Fruits = ["Apple", "Banana", "Mango"];
+console.log(Fruits);
+Fruits[1] = "Orange";
+console.log(Fruits);
