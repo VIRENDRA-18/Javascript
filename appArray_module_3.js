b2c7681b8419e57d443220d1b2a993fbaf654d6b
VIRENDRA-18 <virusingh0125@gmail.com>
2025-12-03 12:14:49 +0530

    Add practice problem for array methods with Months example

--- appArray_module_3.js
+++ appArray_module_3.js
@@ -19,3 +19,14 @@ console.log(players.unshift("Shami", "Bhuvi")); // add element at first
 console.log(players);
 console.log(players.shift()); // remove element from first
 console.log(players);
+
+//Practice Problem
+
+let Months = ["january", "july", "march", "august", "october"];
+
+console.log(Months);
+console.log(Months.shift()); // remove first month
+console.log(Months.shift());
+console.log(Months.unshift("june"));// add june at first
+console.log(Months.unshift("july"));
+console.log(Months);
