48e131584956fd005d2fca13a933c3a3ec70115a
VIRENDRA-18 <virusingh0125@gmail.com>
2025-12-03 12:08:54 +0530

    Add examples for array methods including push, pop, unshift, and shift

--- appArray_module_3.js
+++ appArray_module_3.js
@@ -7,3 +7,15 @@ let Fruits = ["Apple", "Banana", "Mango"];
 console.log(Fruits);
 Fruits[1] = "Orange";
 console.log(Fruits);
+
+let players = ["Virat", "Rohit", "Dhoni", "Jadeja"];
+console.log(players);
+console.log(players.push("Bumrah", "Rana")); // add element at last
+console.log(players);
+console.log(players.pop()); // remove element from last
+console.log(players);
+
+console.log(players.unshift("Shami", "Bhuvi")); // add element at first
+console.log(players);
+console.log(players.shift()); // remove element from first
+console.log(players);
